f4ed2445ff3a4355bbe7588206a5a2dceea9263a
Abdullah <abdullah.khamoor4@gmail.com>
2026-01-15 13:02:58 +0500

    url change in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
         try {
 
             const response = await axios.post(
-                "http://localhost:5000/api/auth/login",
+                "https://ems-backend-weld.vercel.app/api/auth/login",
                 { email, password }
             )
             if (response.data.success) {
