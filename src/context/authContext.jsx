2b163c4e6b5e96fee7cd1c984c655fb4c86ebf13
Abdullah <abdullah.khamoor4@gmail.com>
2026-01-16 10:26:52 +0500

    authcontext to vercel

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -16,7 +16,7 @@ const authContext = ({ children }) => {
             try {
                 const token = localStorage.getItem('token')
                 if (token) {
-                    const response = await axios.get('http://localhost:5000/api/auth/verify', {
+                    const response = await axios.get('https://ems-backend-66z5.vercel.app/api/auth/verify', {
                         headers: {
                             "Authorization": `Bearer ${token}`
                         }
