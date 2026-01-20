746333ac7e534750423ab441584570a5762c4463
Abdullah <abdullah.khamoor4@gmail.com>
2026-01-20 09:31:57 +0500

    list change image url

--- src/components/employee/List.jsx
+++ src/components/employee/List.jsx
@@ -32,7 +32,7 @@ const List = () => {
                             dep_name: emp.department.dep_name,
                             name: emp.userId.name,
                             dob: new Date(emp.dob).toLocaleDateString(),
-                            profileImage: (<img width={40} className='rounded-full' src={`https://ems-backend-66z5.vercel.app/${emp.userId.profileImage}`} />),
+                            profileImage: (<img width={40} className='rounded-full' src={emp.userId.profileImage} />),
                             action: (<EmployeeButtons Id={emp._id} />)
                         }
                     ))
