1a4a45ef2feac119fa396042567a11388d0b8727
Abdullah <abdullah.khamoor4@gmail.com>
2026-01-20 10:50:37 +0500

    image width

--- src/components/employeeDashboard/Summary.jsx
+++ src/components/employeeDashboard/Summary.jsx
@@ -40,7 +40,7 @@ const Summary = () => {
 
                     <div className={`text-3xl flex justify-center items-center bg-teal-600 text-white px-4`}>
                         {/* <FaUser /> */}
-                        <img src={employee.userId.profileImage} alt="" className='rounded-full w-10 h-10' />
+                        <img src={employee.userId.profileImage} alt="" className='rounded-full w-15 h-15' />
                     </div>
                     <div className='pl-4 py-1'>
                         <p className='text-lg font-semibold'>Welcome Back</p>
