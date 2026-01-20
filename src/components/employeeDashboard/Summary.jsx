11a1dc8ae7ca3c276f2d6b4e8989602b1f575c70
Abdullah <abdullah.khamoor4@gmail.com>
2026-01-20 10:02:31 +0500

    employeeDashboard summary image add

--- src/components/employeeDashboard/Summary.jsx
+++ src/components/employeeDashboard/Summary.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { FaUser } from 'react-icons/fa'
 import { useAuth } from '../../context/authContext'
 
 const Summary = () => {
     const { user } = useAuth()
+
+    const [employee, setEmployee] = useState(null)
+
+    useEffect(() => {
+        const fetchEmployee = async () => {
+
+            try {
+                const response = await axios.get(`https://ems-backend-66z5.vercel.app/api/employee/${id}`, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    }
+
+                })
+                if (response.data.success) {
+                    setEmployee(response.data.employee)
+                }
+            } catch (error) {
+                if (error.response && !error.response.data.success) {
+                    alert(error.response.data.error)
+                }
+            }
+
+        };
+
+        fetchEmployee();
+    }, []);
+
     return (
         <div className='p-6'>
             <div className='rounded flex bg-white'>
 
                 <div className={`text-3xl flex justify-center items-center bg-teal-600 text-white px-4`}>
                     <FaUser />
+                    <img src={employee.userId.profileImage} alt="" />
                 </div>
                 <div className='pl-4 py-1'>
                     <p className='text-lg font-semibold'>Welcome Back</p>
